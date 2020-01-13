806d069ae8319c37f0c89e24ece4dd48e5b8ac37
Martyn Janes <martyn@obany.com>
2020-01-13 13:38:59 +0100

    Added missing type exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from "./mam/channel";
 export * from "./mam/client";
 export * from "./mam/parser";
+export * from "./models/IMamChannelState";
+export * from "./models/IMamFetchedMessage";
+export * from "./models/IMamMessage";
+export * from "./models/mamMode";
